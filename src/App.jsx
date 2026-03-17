b68510bddabe13831159d12bb6cd2afee6005919
Blake Simpson <iblakesimpson@gmail.com>
2026-03-18 12:15:54 +1300

    Add Hero component with typewriter effect and introductory text

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Typewriter } from "react-simple-typewriter";
+import Hero from "./Components/Hero";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-
-      <div className="absolute top-[22%] left-[15%]">
-        <h1 className="text-accent font-poppins text-[clamp(4rem,8vw,6rem)]">
-        <Typewriter
-          words={[
-            "Hello",
-            "Hola",
-            "Bonjour",
-            "Hallo",
-            "Ciao",
-            "Olá",
-            "नमस्ते",
-            "こんにちは",
-            "你好",
-            "안녕하세요",
-          ]}
-          loop={0} // infinite
-          cursor
-          typeSpeed={180}
-          deleteSpeed={100}
-          delaySpeed={2000}
-        />
-        </h1>
-      </div>
+      <Hero />
     </>
   );
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="absolute top-[22%] left-[15%]">
+        <h1 className="text-accent font-poppins text-8xl">
+          <Typewriter
+            words={[
+              "Hello",
+              "Hola",
+              "Bonjour",
+              "Hallo",
+              "Ciao",
+              "Olá",
+              "नमस्ते",
+              "こんにちは",
+              "你好",
+              "안녕하세요",
+            ]}
+            loop={0} // infinite
+            cursor
+            typeSpeed={180}
+            deleteSpeed={100}
+            delaySpeed={2000}
+          />
+        </h1>
+
+        <h2 className="font-main font-normal text-white mt-6 text-4xl">
+          I'm Blake
+        </h2>
+        <p className="font-poppins text-grey-300 max-w-md text-wrap mt-2 mr-10">
+          A Computer Science student at the University of Auckland, graduating
+          in 2027
+        </p>
+        <button className="border font-main font-light text-white border-accent px-4 py-2 mt-4 hover:bg-accent/20 transition">
+          See my projects
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
