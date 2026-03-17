ff7d7700c296028a7537217d047454c82a3914a7
Blake Simpson <iblakesimpson@gmail.com>
2026-03-18 11:06:10 +1300

    Basic styles - typewriter homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Typewriter } from "react-simple-typewriter";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="absolute top-[22%] left-[15%]">
+        <h1 className="text-accent font-poppins text-[clamp(4rem,8vw,6rem)]">
+        <Typewriter
+          words={[
+            "Hello",
+            "Hola",
+            "Bonjour",
+            "Hallo",
+            "Ciao",
+            "Olá",
+            "नमस्ते",
+            "こんにちは",
+            "你好",
+            "안녕하세요",
+          ]}
+          loop={0} // infinite
+          cursor
+          typeSpeed={180}
+          deleteSpeed={100}
+          delaySpeed={2000}
+        />
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
